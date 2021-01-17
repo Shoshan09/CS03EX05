fab867181e980a764438ef68062043a5fb19370c
Shoshan09 <48455076+Shoshan09@users.noreply.github.com>
2021-01-17 18:39:10 +0200

    Update WHO.h

--- WHO.h
+++ WHO.h
@@ -10,30 +10,37 @@ class WHO
 {
 public:
 	WHO();
-	WHO(Covid19** panArray, const char* workCountry, int totalPop, int totalSick, int DiffViruse);
+	WHO(const char* workCountry, int totalPop, int totalSick, int DiffViruse);
 	WHO(const WHO& other);
 	~WHO();
 	
-	int setCovidArray(Covid** panArray);
+	//setters
+	int setCovidArray(Covid19** panArray, int size);
 	int setWorkCountry(const char* workCountry);
 	int setTotalPop(int totalPop);
 	int setTotalSick(int totalSick);
 	int setDiffViruses(int DiffViruses);
 
+	//getters
 	char* getCountry();
 	int getTotalPop();
 	int getTotalSick();
 	int getDiffViruses();
+	Covid19** getPandemicArray();
 
+	//different functions
+	Covid19** addToArray(Covid19 cov);
 	int countViruses(); //functinon to count number of different viruses in the country and change the variable m_differentViruses accordingly
-	int changeKZGenSeq(const char* genSeq);
+	int changeKZGenSeq(int index, const char* genSeq);
 
+	void print();
 	void operator=(const WHO& other);
 	friend ostream& operator<<(ostream& os, const WHO& who);
 
 
 private:
 	Covid19** m_pandemicArray;
+	int m_arraySize;
 	char* m_workCountry;
 	int m_totalPopulation;
 	int m_totalSick;
