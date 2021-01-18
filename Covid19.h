61214b874edcfc6e987600962871fa13160bc361
Shoshan09 <48455076+Shoshan09@users.noreply.github.com>
2021-01-18 15:31:44 +0200

    Update Covid19.h

--- Covid19.h
+++ Covid19.h
@@ -6,13 +6,16 @@ class Covid19
 public:
 	Covid19();
 	Covid19(float R, int bio, bool vac, const char* serial);
-	~Covid19() { delete[] m_serial; }
+	Covid19(const Covid19& other);
+	virtual ~Covid19() { delete[] m_serial; }
 
+	//setters
 	float setR(float R);
 	int setMoleculeNum(int bioMoleculeNum);
 	int setVaccine(bool Vaccine = false);
 	int setSerial(const char* serial);
 
+	//getters
 	float getR();
 	int getMoleculeNum();
 	bool getVaccine();
@@ -20,7 +23,7 @@ public:
 
 	void print();
 
-private:
+protected:
 	float m_R;
 	int m_bioMoleculeNum;
 	bool m_vaccine;
