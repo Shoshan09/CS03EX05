51bb65069082ed50cb0a473dea15bbdf74fd13e6
Shoshan09 <48455076+Shoshan09@users.noreply.github.com>
2021-01-19 14:55:22 +0200

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,4 @@
-#include "Covid19.h"
-#include "CovidGB.h"
-#include "CovidSA.h"
-#include "CovidKZ.h"
+#include "WHO.h"
 #include <iostream>
 
 using namespace std;
@@ -9,9 +6,34 @@ using namespace std;
 int main()
 {
 
-	CovidKZ kz(1.2, 3, true, "ABCD", 2.3, "gb", 1.7, "sa", "TBCJA");
+	Covid19 co19(2.2, 4, true, "fkal");
+	CovidGB gb(1.5, 4, false, "defg", 3.5, "BUTSWANA");
+	CovidSA sa(3.1, 415, false, "ADDQD", 8.1, "Cape Verde");
+	CovidKZ kz(1.2, 3, true, "ABCD", 2.3, "PALMA", 1.7, "sa", "DGGD");
 
-	kz.print();
+	Covid19* cov[4];
+	
+	cov[0] = new Covid19(co19);
+	cov[1] = new CovidGB(gb);
+	cov[2] = new CovidKZ(kz);
+	cov[3] = new CovidSA(sa);
+	
+	
+	//initilzing WHO class
+	WHO w("Butswana", 1500, 400, 2, cov, 4);
+
+	//printing before KZ genetic sequance change
+	w.print();
+
+	//tyring to change - not working
+	w.changeKZGenSeq(1,"ABABA");
+
+	//trying to change - working
+	w.changeKZGenSeq(2, "ABABA");
+
+	//printing after change
+	cout << "\n";
+	w.print();
 
 	return 0;
 }
